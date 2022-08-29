3e2a8dc7c01b241d6b56296d6f23050d217aac11
Quade Houston <quadehouston@gmail.com>
2022-08-29 13:00:22 -0400

    commit tree 80dcada380d5052da6d6c8aa81c724f8001ec27e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badges)
+    badges.each { |n| puts "Hello, my name is #{n}." }
+end
+binding.pry
